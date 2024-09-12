a901bca5cb1ba3a041dc533f4454d5d71849de4e
Vishnu Koundinya Challa <104033598+Cvk37@users.noreply.github.com>
2024-09-12 12:06:57 -0500

    Delete products/src/main/java/com/vk/products/jwt/jwtConfig.java

--- products/src/main/java/com/vk/products/jwt/jwtConfig.java
+++
@@ -1,7 +0,0 @@
-package com.vk.products.jwt;
-
-public class jwtConfig {
-        public static final String jwtSecret = "0oYwmQzk9zlEt4gYzjh9IjX6LYFZjjf5vp/ukbYu+Ss=" ;
-        public static final int jwtExpirationMs= 7200000;
-
-}
